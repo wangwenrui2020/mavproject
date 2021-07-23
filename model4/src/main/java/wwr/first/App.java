9cfe960ff3083f957eb150f9119862f5a901fa73
wwr <wangwenr@eastcom.com>
2021-07-23 12:02:34 +0800

    Initial commit

---
+++ model4/src/main/java/wwr/first/App.java
@@ -0,0 +1,13 @@
+package wwr.first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
